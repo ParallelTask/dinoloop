2d3f4d85cc78f190a3dae206442d41f934eb5827
Dinoloop <kkonkula1192@gmail.com>
2018-07-15 23:32:43 +0530

    fixed express runtime error

--- examples/parse_api/src/app/middlewares/filters.ts
+++ examples/parse_api/src/app/middlewares/filters.ts
@@ -7,13 +7,17 @@ import {
 export class ServerParseErrorMiddleware extends ErrorMiddleware {
     invoke(err: Error, request, response, next): void {
         if (err instanceof ParseParamException) next(err);
-        response.json(`${err.message} - Ended by Server level middleware`);
+        else {
+            response.json(`${err.message} - Ended by Server level middleware`);
+        }
     }
 }
 
 export class ControllerParseErrorMiddleware extends ExceptionFilter {
     invoke(err: Error, request, response, next): void {
         if (err instanceof ParseParamException) next(err);
-        response.json(`${err.message} - Ended by Controller Exception Filter`);
+        else {
+            response.json(`${err.message} - Ended by Controller Exception Filter`);
+        }
     }
 }
